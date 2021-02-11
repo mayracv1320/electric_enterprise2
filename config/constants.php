3d33df6788c3e1b59ffe06f41d57863398de3431
Mayra Alexandra Castro Vargas <mayra1320.92@gmail.com>
2021-02-11 15:27:48 -0500

    mayraaaaa

--- config/constants.php
+++ config/constants.php
@@ -1,9 +1,9 @@
 <?php
 
-define('HOST', 'bcrpyeszazfgqo2x2s7b-mysql.services.clever-cloud.com');
-define('USER', 'umecbng9usujxe2v');
-define('PASSWORD', 'rjqUNYc2cmUzQGKNPuTL');
-define('DATABASE_NAME', 'bcrpyeszazfgqo2x2s7b');
+define('HOST', 'bu80vbqxziimm0fg99tx-mysql.services.clever-cloud.com');
+define('USER', 'u2n2zntjcaxqdaa5');
+define('PASSWORD', 'ktbgJ5QGLupSDyEdURTc');
+define('DATABASE_NAME', 'bu80vbqxziimm0fg99tx');
 
 define('CURRENCY', 'S/.');
 
