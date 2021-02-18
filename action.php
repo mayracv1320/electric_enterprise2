14d48630ace44d34f41a55e8524fb7d502d83e77
Mayra Alexandra Castro Vargas <mayra1320.92@gmail.com>
2021-02-18 02:41:33 -0500

    payment_su

--- action.php
+++ action.php
@@ -302,9 +302,9 @@ if (isset($_POST["Common"])) {
 								}
 							  
 							echo   
-								'<input type="hidden" name="return" value="http://localhost/electric_enterprise2/payment_success.php"/>
-					                <input type="hidden" name="notify_url" value="http://localhost/electric_enterprise2/payment_success.php">
-									<input type="hidden" name="cancel_return" value="http://localhost/electric_enterprise2/cancel.php"/>
+								'<input type="hidden" name="return" value="https://electric-enterprise.000webhostapp.com/payment_success.php"/>
+					                <input type="hidden" name="notify_url" value="https://electric-enterprise.000webhostapp.com/payment_success.php">
+									<input type="hidden" name="cancel_return" value="https://electric-enterprise.000webhostapp.com/cancel.php"/>
 									<input type="hidden" name="currency_code" value="USD"/>
 									<input type="hidden" name="custom" value="'.$_SESSION["uid"].'"/>
 									<input style="float:right;margin-right:80px;" type="image" name="submit"
